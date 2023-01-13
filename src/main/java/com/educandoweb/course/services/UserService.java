33bd25007e20b37ee3c9aef623f0c8fff6d375d4
Maicou Hahn <maicouhahn11@gmail.com>
2023-01-13 09:52:40 -0300

    Service layer, component registration

---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+//@Component//registra a classe como componente do springboot
+@Service//é a mesma coisa que o component mas a palavra é semanticamente melhor
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	public List<User>findAll(){
+		
+		return repository.findAll();
+	}
+}

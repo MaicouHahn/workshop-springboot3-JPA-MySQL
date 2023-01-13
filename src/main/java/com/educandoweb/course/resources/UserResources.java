f2c063cc09f58b9a657e204bd30525cef799ec77
Maicou Hahn <maicouhahn11@gmail.com>
2023-01-13 07:37:57 -0300

    User Entity and Resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResources.java
@@ -0,0 +1,21 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		//retorna o objeto no padrao json para o endereço /users
+		User u = new User(1L,"Maria","maria@gmail.com","000000000","12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
